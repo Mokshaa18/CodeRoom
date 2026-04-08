3354b1f7fb318cb5c915cfdc1949970c28e140c3
Mokshagna <moksha7691@gmail.com>
2026-04-08 10:59:26 +0000

    refactor: organize backend with Spring Boot structure

---
+++ coderoom-backen/src/main/java/com/coderoom/coderoom_backen/CoderoomBackenApplication.java
@@ -0,0 +1,13 @@
+package com.coderoom.coderoom_backen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderoomBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderoomBackenApplication.class, args);
+	}
+
+}
---
+++ coderoom-backen/src/test/java/com/coderoom/coderoom_backen/CoderoomBackenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderoom.coderoom_backen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderoomBackenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
